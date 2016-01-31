60364a3e566f6cec5c4bac1a922c794337567faf
Ivan Demabildo <ivan_demabildo@dlsu.edu.ph>
2016-01-31 11:01:05 +0800

    Updated Error Configuration

--- src/main/java/EnvironmentConfiguration/model/error_logging/InstructionInvalid.java
+++ src/main/java/EnvironmentConfiguration/model/error_logging/InstructionInvalid.java
@@ -10,7 +10,7 @@ public enum InstructionInvalid {
     invalidDuplicateFileRegisterDestinationParameter,
     invalidFileNoSuchAddressingVariable,
     invalidFileExceededParameterCount,
-    invalidFileLackingParameterCount
-
+    invalidFileLackingParameterCount,
+    invalidParameterFormat
 }
 
