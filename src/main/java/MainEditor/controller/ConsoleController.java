d78cc349d0a7f75c7f36e0c02bd6ea57aac19fdc
Marielle Ong <marielletong@gmail.com>
2016-02-08 10:32:32 +0800

    console

--- src/main/java/MainEditor/controller/ConsoleController.java
+++ src/main/java/MainEditor/controller/ConsoleController.java
@@ -1,18 +1,26 @@
 package MainEditor.controller;
 
+import EnvironmentConfiguration.model.engine.Register;
+import MainEditor.model.AssemblyComponent;
+import MainEditor.model.FlagUI;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Tab;
+import javafx.scene.control.cell.PropertyValueFactory;
 import org.fxmisc.richtext.StyleClassedTextArea;
 import org.fxmisc.richtext.StyleSpans;
 import org.fxmisc.richtext.StyleSpansBuilder;
 import org.fxmisc.richtext.StyledTextArea;
 
-import java.util.Collection;
-import java.util.Collections;
+import java.net.URL;
+import java.util.*;
 
 /**
  * Created by Jennica on 07/02/2016.
  */
-public class ConsoleController {
+public class ConsoleController extends AssemblyComponent implements Initializable {
 
     private StyledTextArea textArea;
     private Tab tab;
@@ -37,5 +45,57 @@ public class ConsoleController {
         spansBuilder.add(Collections.singleton("console-font-color"), text.length());
 
         return spansBuilder.create();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @Override
+    public void build(){
+
+    }
+
+    @Override
+    public void update(String currentLine, int lineNumber) {
+
+    }
+
+    @Override
+    public void refresh() {
+
+    }
+
+    /*
+     * print
+     */
+    public void printf(String text) {
+        textArea.appendText(text);
+
+    }
+
+    /*
+     * scan
+     */
+    public String scanf(int start, int end) {
+        return textArea.getText(start, end);
+
+    }
+
+    /*
+     * clear screen
+     */
+    public void clear() {
+        textArea.clear();
+
+    }
+
+    /*
+     * get char
+     */
+    public String getch(int start, int end, int index) {
+        return textArea.getText(start, end).charAt(index) + "";
+
     }
 }
