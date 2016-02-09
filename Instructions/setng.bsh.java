92df578c83fe2d40af21f567e05d713e148de993
Marielle Ong <marielletong@gmail.com>
2016-02-09 13:56:45 +0800

    DONE SETcc

---
+++ Instructions/setng.bsh.java
@@ -0,0 +1,25 @@
+execute(des, registers, memory){
+	Calculator cal = new Calculator(registers, memory);
+	boolean initialCondition = cal.evaluateCondition("NG");
+	System.out.println("Should I set: " + initialCondition);
+
+	/**
+	 * If condition is true, carry out the move
+	 */
+	if ( initialCondition ) {
+		if ( des.isRegister() ) {
+			registers.set(des, "1");
+		}
+		else if ( des.isMemory() ) {
+			memory.write(des, "1", des);
+		}
+	}
+	else {
+		if ( des.isRegister() ) {
+			registers.set(des, "0");
+		}
+		else if ( des.isMemory() ) {
+			memory.write(des, "0", des);
+		}
+	}
+}
