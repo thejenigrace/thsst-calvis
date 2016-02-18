5325d3cccb6635f8fdc7dd47fcbeacda2bb8b186
Jennica Alcalde <jennica.alcalde@gmail.com>
2016-02-18 12:16:23 +0800

    Fix DIV & IDIV Instructions

--- Instructions/div.bsh.java
+++ Instructions/div.bsh.java
@@ -2,6 +2,7 @@ execute(src, registers, memory) {
         int srcBitSize;
         String x = "";
         EFlags ef=registers.getEFlags();
+        Calculator c=new Calculator(registers, memory);
         if ( src.isRegister() ){
             System.out.println("DIV src = register");
             srcBitSize = registers.getBitSize(src);
@@ -19,40 +20,56 @@ execute(src, registers, memory) {
                 BigInteger biX = new BigInteger(x, 16);
                 BigInteger biY = new BigInteger(registers.get("AX"), 16);
                 BigInteger[] result = biY.divideAndRemainder(biX);
-                registers.set("AL", result[0].toString(16));
-                registers.set("AH", result[1].toString(16));
+                String quotient = c.cutToCertainHexSize(result[0].toString(16), 2);
+                String remainder = c.cutToCertainHexSize(result[1].toString(16), 2);
+                registers.set("AL", quotient);
+                registers.set("AH", remainder);
 
                 // debugging
                 System.out.println("AX = " + biY.toString(16));
                 System.out.println("src (byte) = " + biX.toString(16));
                 System.out.println("quotient = " + result[0].toString(16));
                 System.out.println("remainder = " + result[1].toString(16));
+                System.out.println("quotient = " + quotient);
+                System.out.println("remainder = " + remainder);
             } else if ( srcBitSize == 16 ){
                 BigInteger biX = new BigInteger(x, 16);
                 String y = registers.get("DX") + registers.get("AX");
                 BigInteger biY = new BigInteger(y, 16);
                 BigInteger[] result = biY.divideAndRemainder(biX);
-                registers.set("AX", result[0].toString(16));
-                registers.set("DX", result[1].toString(16));
+//                registers.set("AX", result[0].toString(16));
+//                registers.set("DX", result[1].toString(16));
+                String quotient = c.cutToCertainHexSize(result[0].toString(16), 4);
+                String remainder = c.cutToCertainHexSize(result[1].toString(16), 4);
+                registers.set("AX", quotient);
+                registers.set("DX", remainder);
 
                 // debugging
                 System.out.println("DX, AX = " + y);
                 System.out.println("src (word) = " + biX.toString(16));
                 System.out.println("quotient = " + result[0].toString(16));
                 System.out.println("remainder = " + result[1].toString(16));
+                System.out.println("quotient = " + quotient);
+                System.out.println("remainder = " + remainder);
             } else if ( srcBitSize == 32 ){
                 BigInteger biX = new BigInteger(x, 16);
                 String y = registers.get("EDX") + registers.get("EAX");
                 BigInteger biY = new BigInteger(y, 16);
                 BigInteger[] result = biY.divideAndRemainder(biX);
-                registers.set("EAX", result[0].toString(16));
-                registers.set("EDX", result[1].toString(16));
+//                registers.set("EAX", result[0].toString(16));
+//                registers.set("EDX", result[1].toString(16));
+                String quotient = c.cutToCertainHexSize(result[0].toString(16), 8);
+                String remainder = c.cutToCertainHexSize(result[1].toString(16), 8);
+                registers.set("EAX", quotient);
+                registers.set("EDX", remainder);
 
                 // debugging
                 System.out.println("EDX, EAX = " + y);
                 System.out.println("src (dword) = " + biX.toString(16));
                 System.out.println("quotient = " + result[0].toString(16));
                 System.out.println("remainder = " + result[1].toString(16));
+                System.out.println("quotient = " + quotient);
+                System.out.println("remainder = " + remainder);
             }
 //        }
  }
