3432a809692e40cd6bfd9d263857f76bdf64769f
Jennica Alcalde <jennica.alcalde@gmail.com>
2016-03-05 00:22:57 +0800

    Added PCMPGT(B/W/D) Instructions

---
+++ Instructions/pcmpgtd.bsh.java
@@ -0,0 +1,23 @@
+execute(des,src,registers,memory) {
+    int srcBitSize;
+    int srcHexSize;
+    String srcValue;
+    String desValue = registers.get(des);
+    Calculator calculator = new Calculator(registers,memory);
+
+    if (src.isRegister()) {
+        srcBitSize = registers.getBitSize(src);
+        srcHexSize = registers.getHexSize(src);
+        srcValue = registers.get(src);
+    } else if (src.isMemory()) {
+        srcBitSize = memory.getBitSize(src);
+        srcHexSize = memory.getHexSize(src);
+        srcValue = memory.read(src,srcBitSize);
+    }
+
+    // src == mm || src == xmm
+    if (srcBitSize == 64 || srcBitSize == 128) {
+        String result = calculator.cutBySizeAndCompare(desValue, srcValue, srcHexSize, 8, 'g');
+        registers.set(des, result);
+    }
+}
