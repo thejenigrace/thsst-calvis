df4a782b969c082549e73b256bccd88ddbae26c1
Jennica Alcalde <jennica.alcalde@gmail.com>
2016-03-08 10:37:42 +0800

    Added (SSE) MOVAPS & MOVUPS Instructions

---
+++ Instructions/movaps.bsh.java
@@ -0,0 +1,29 @@
+execute(des,src,registers,memory) {
+    int desBitSize;
+    int desHexSize;
+    String desValue;
+    String srcValue;
+
+    if(des.isRegister()) {
+        desBitSize = registers.getBitSize(des);
+        desHexSize = registers.getHexSize(des);
+    } else if(des.isMemory()) {
+        desBitSize = memory.getBitSize(des);
+        desHexSize = memory.getHexSize(des);
+    }
+
+    if(src.isRegister()) {
+        srcValue = registers.get(src);
+    } else if(src.isMemory()) {
+        // System.out.println("srcBaseAddress = " + memory.getBaseAddress(src));
+        srcValue = memory.read(src,desBitSize);
+    }
+
+    if(des.isRegister()) {
+        registers.set(des, srcValue);
+    } else if(des.isMemory()) {
+        // System.out.println("desBaseAddress = " + memory.removeSizeDirectives());
+
+        memory.write(des, srcValue, des);
+    }
+}
