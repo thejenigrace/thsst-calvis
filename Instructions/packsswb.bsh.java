0f531cd75990b4579358503239b3f657e659e66a
Marielle Ong <marielletong@gmail.com>
2016-03-11 10:04:20 +0800

    mmx conversion

--- Instructions/packsswb.bsh.java
+++ Instructions/packsswb.bsh.java
@@ -44,32 +44,16 @@ execute(des, src, registers, memory) {
 storeResultToRegister(registers, calculator, des, source, destination, desSize) {
     String result = "";
 
+    Token token = new Token(Token.REG, "AX");
+
     BigInteger biDes = new BigInteger("0", 16);
     BigInteger wordpos = new BigInteger("7F", 16);
-    BigInteger wordneg = new BigInteger("80", 16);
+
+    int intVal = Integer.parseInt("80", 16);
+    String neg = Integer.toHexString((-1 * intVal));
+    BigInteger wordneg = new BigInteger(neg, 16);
 
     if( desSize == 64 ) {
-        for(int i = 0; i <= 12; i+=4) {
-            if( i == 12 ) {
-                biDes = new BigInteger(destination.substring(i), 16);
-            }
-            else {
-                biDes = new BigInteger(destination.substring(i, i + 4), 16);
-            }
-
-            if( biDes.compareTo(wordpos) == 1 ) {
-                result += "7F";
-            }
-            else {
-                if( i == 12 ) {
-                    result += destination.substring(i + 2);
-                }
-                else {
-                    result += destination.substring(i + 2, i + 4);
-                }
-            }
-        }
-
         for(int i = 0; i <= 12; i+=4) {
             if( i == 12 ) {
                 biDes = new BigInteger(source.substring(i), 16);
@@ -78,15 +62,78 @@ storeResultToRegister(registers, calculator, des, source, destination, desSize) 
                 biDes = new BigInteger(source.substring(i, i + 4), 16);
             }
 
-            if( biDes.compareTo(wordpos) == 1 ) {
-                result += "7F";
-            }
-            else {
-                if( i == 12 ) {
-                    result += source.substring(i + 2);
+            String temp = calculator.hexToBinaryString(biDes.toString(16), token);
+            String sign = temp.charAt(0) + "";
+
+            if( sign.equals("0") ) {
+                if( biDes.compareTo(wordpos) == 1 ) {
+                    result += "7F";
                 }
                 else {
-                    result += source.substring(i + 2, i + 4);
+                    if( i == 12 ) {
+                        result += source.substring(i + 2);
+                    }
+                    else {
+                        result += source.substring(i + 2, i + 4);
+                    }
+                }
+            } else if( sign.equals("1") ) {
+                int intVal = Integer.parseInt(biDes.toString(16), 16);
+                String twosComplement = Integer.toHexString((-1 * intVal));
+                biDes = new BigInteger(twosComplement, 16);
+
+                if( biDes.compareTo(wordneg) == -1 ) {
+                    result += "80";
+                }
+                else {
+                    if( i == 12 ) {
+                        result += source.substring(i + 2);
+                    }
+                    else {
+                        result += source.substring(i + 2, i + 4);
+                    }
+                }
+            }
+        }
+
+        for(int i = 0; i <= 12; i+=4) {
+            if( i == 12 ) {
+                biDes = new BigInteger(destination.substring(i), 16);
+            }
+            else {
+                biDes = new BigInteger(destination.substring(i, i + 4), 16);
+            }
+
+            String temp = calculator.hexToBinaryString(biDes.toString(16), token);
+            String sign = temp.charAt(0) + "";
+
+            if( sign.equals("0") ) {
+                if( biDes.compareTo(wordpos) == 1 ) {
+                    result += "7F";
+                }
+                else {
+                    if( i == 12 ) {
+                        result += destination.substring(i + 2);
+                    }
+                    else {
+                        result += destination.substring(i + 2, i + 4);
+                    }
+                }
+            } else if( sign.equals("1") ) {
+                int intVal = Integer.parseInt(biDes.toString(16), 16);
+                String twosComplement = Integer.toHexString((-1 * intVal));
+                biDes = new BigInteger(twosComplement, 16);
+
+                if( biDes.compareTo(wordneg) == -1 ) {
+                    result += "80";
+                }
+                else {
+                    if( i == 12 ) {
+                        result += destination.substring(i + 2);
+                    }
+                    else {
+                        result += destination.substring(i + 2, i + 4);
+                    }
                 }
             }
         }
@@ -94,42 +141,84 @@ storeResultToRegister(registers, calculator, des, source, destination, desSize) 
     else if( desSize == 128 ) {
         for(int i = 0; i <= 28; i+=4) {
             if( i == 28 ) {
-                biDes = new BigInteger(destination.substring(i), 16);
-            }
-            else {
-                biDes = new BigInteger(destination.substring(i, i + 4), 16);
-            }
-
-            if( biDes.compareTo(wordpos) == 1 ) {
-                result += "7F";
-            }
-            else {
-                if( i == 12 ) {
-                    result += destination.substring(i + 2);
-                }
-                else {
-                    result += destination.substring(i + 2, i + 4);
-                }
-            }
-        }
-
-        for(int i = 0; i <= 28; i+=4) {
-            if( i == 28 ) {
                 biDes = new BigInteger(source.substring(i), 16);
             }
             else {
                 biDes = new BigInteger(source.substring(i, i + 4), 16);
             }
 
-            if( biDes.compareTo(wordpos) == 1 ) {
-                result += "7F";
-            }
-            else {
-                if( i == 12 ) {
-                    result += source.substring(i + 2);
+            String temp = calculator.hexToBinaryString(biDes.toString(16), token);
+            String sign = temp.charAt(0) + "";
+
+            if( sign.equals("0") ) {
+                if( biDes.compareTo(wordpos) == 1 ) {
+                    result += "7F";
                 }
                 else {
-                    result += source.substring(i + 2, i + 4);
+                    if( i == 28 ) {
+                        result += source.substring(i + 2);
+                    }
+                    else {
+                        result += source.substring(i + 2, i + 4);
+                    }
+                }
+            } else if( sign.equals("1") ) {
+                int intVal = Integer.parseInt(biDes.toString(16), 16);
+                String twosComplement = Integer.toHexString((-1 * intVal));
+                biDes = new BigInteger(twosComplement, 16);
+
+                if( biDes.compareTo(wordneg) == -1 ) {
+                    result += "80";
+                }
+                else {
+                    if( i == 28 ) {
+                        result += source.substring(i + 2);
+                    }
+                    else {
+                        result += source.substring(i + 2, i + 4);
+                    }
+                }
+            }
+        }
+
+        for(int i = 0; i <= 28; i+=4) {
+            if( i == 28 ) {
+                biDes = new BigInteger(destination.substring(i), 16);
+            }
+            else {
+                biDes = new BigInteger(destination.substring(i, i + 4), 16);
+            }
+
+            String temp = calculator.hexToBinaryString(biDes.toString(16), token);
+            String sign = temp.charAt(0) + "";
+
+            if( sign.equals("0") ) {
+                if( biDes.compareTo(wordpos) == 1 ) {
+                    result += "7F";
+                }
+                else {
+                    if( i == 28 ) {
+                        result += destination.substring(i + 2);
+                    }
+                    else {
+                        result += destination.substring(i + 2, i + 4);
+                    }
+                }
+            } else if( sign.equals("1") ) {
+                int intVal = Integer.parseInt(biDes.toString(16), 16);
+                String twosComplement = Integer.toHexString((-1 * intVal));
+                biDes = new BigInteger(twosComplement, 16);
+
+                if( biDes.compareTo(wordneg) == -1 ) {
+                    result += "80";
+                }
+                else {
+                    if( i == 28 ) {
+                        result += destination.substring(i + 2);
+                    }
+                    else {
+                        result += destination.substring(i + 2, i + 4);
+                    }
                 }
             }
         }
