405102874a9c10a6d77c0d297ee91fe1b00969ea
Ivan Demabildo <ivan_demabildo@dlsu.edu.ph>
2016-07-12 15:32:43 +0800

    ok animation

--- src/main/java/simulatorvisualizer/model/instructionanimation/Loope.java
+++ src/main/java/simulatorvisualizer/model/instructionanimation/Loope.java
@@ -14,12 +14,15 @@ import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.util.Duration;
 import simulatorvisualizer.model.CalvisAnimation;
+import simulatorvisualizer.model.JumperFunction;
 import simulatorvisualizer.model.TimeLineFunction;
+
+import java.util.ArrayList;
 
 /**
  * Created by Goodwin Chua on 9 Jul 2016.
  */
-public class Je extends CalvisAnimation {
+public class Loope extends CalvisAnimation {
 
     @Override
     public void animate(ScrollPane tab) {
@@ -31,19 +34,31 @@ public class Je extends CalvisAnimation {
         Calculator c = new Calculator(registers, memory);
         Token[] tokens = currentInstruction.getParameterTokens();
         Token des = tokens[0];
+        Token cc = tokens[0];
         String labelName = timeFunc.getValue(des, 0);
+        String jumpMode = "LOOPE";
         ObservableList<Node> parent = this.root.getChildren();
 
         String conditionalTextResult = "";
         String proceedOrNot = "";
-        if( registers.getEFlags().getZeroFlag().equals("1") ){
-            conditionalTextResult = "ZF == 0";
-            proceedOrNot = "Proceed To";
+
+        JumperFunction jc = new JumperFunction(registers, memory);
+        ArrayList<String> getFlags = jc.getFlags(jumpMode);
+        String[] flagValues = new String[getFlags.size()];
+        flagValues = getFlags.toArray(flagValues);
+        boolean isPassable = jc.computeJump(jumpMode, flagValues);
+
+        if(isPassable){
+                conditionalTextResult = jc.computeJumpText(isPassable, jumpMode);
+                proceedOrNot = "Proceed To";
         }
         else{
-            conditionalTextResult = "ZF != 0";
-            proceedOrNot = "Don't Proceed To";
+                conditionalTextResult = jc.computeJumpText(isPassable, jumpMode);
+                proceedOrNot = "Don't Proceed To";
         }
+
+        System.out.println(conditionalTextResult);
+        System.out.println(proceedOrNot);
 
         Rectangle fake = timeFunc.createRectangle(0, 0, Color.WHITE);
         Rectangle fromRec = timeFunc.createRectangle(140, 130, Color.web("#002e63"));
@@ -64,27 +79,32 @@ public class Je extends CalvisAnimation {
         conditionLabel.setY( 60);
 
         conditionVal.setX(30 + fromRec.getLayoutBounds().getWidth()/2 - conditionVal.getLayoutBounds().getWidth()/4 - 10 );
-        conditionVal.setWrappingWidth(fromRec.getLayoutBounds().getWidth() - 10);
+        conditionVal.setWrappingWidth(fromRec.getLayoutBounds().getWidth() - 40);
         conditionVal.setY( 60 + 40);
 
-        labelRec.setX(30 + 320);
+        labelRec.setX(30 + 350);
         labelRec.setY(30);
 
-        conditionLabelLabel.setX( 30 + labelRec.getLayoutBounds().getWidth()/2 - conditionLabelLabel.getLayoutBounds().getWidth() / 2 + 320);
+        conditionLabelLabel.setX( 30 + labelRec.getLayoutBounds().getWidth()/2 - conditionLabelLabel.getLayoutBounds().getWidth() / 2 + 350);
         conditionLabelLabel.setY( 60);
 
-        conditioLabelnVal.setX(30 + labelRec.getLayoutBounds().getWidth()/2 - conditioLabelnVal.getLayoutBounds().getWidth()/4 - 10 + 320);
+        conditioLabelnVal.setX(30 + labelRec.getLayoutBounds().getWidth()/2 - conditioLabelnVal.getLayoutBounds().getWidth()/4 - 10 + 350);
         conditioLabelnVal.setWrappingWidth(labelRec.getLayoutBounds().getWidth() - 10);
         conditioLabelnVal.setY( 60 + 40);
         parent.addAll(labelRec, conditionLabelLabel, conditioLabelnVal);
 
-        acceptanceVal.setX(200);
+        acceptanceVal.setX(labelRec.getX()/2 - fromRec.getX()/2);
         acceptanceVal.setY(95);
 
         FadeTransition textFade = new FadeTransition(Duration.millis(1000), acceptanceVal);
         timeFunc.fadeText(textFade, acceptanceVal, 1.0, 0.1);
         textFade.play();
-
+        if(isPassable){
+            acceptanceVal.setFill(Color.web("#0bda51"));
+        }
+        else{
+            acceptanceVal.setFill(Color.web("#e62020"));
+        }
         timeline.setDelay(Duration.millis(2000));
         timeline.setCycleCount(1);
         timeline.play();
