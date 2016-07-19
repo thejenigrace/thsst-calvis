47b3edea71090b4fd1d93a1c70a23f58f315c5fb
Marielle Ong <marielletong@gmail.com>
2016-07-20 01:31:43 +0800

    not done

--- src/main/java/thsst/calvis/simulatorvisualizer/model/instructionanimation/Psllw.java
+++ src/main/java/thsst/calvis/simulatorvisualizer/model/instructionanimation/Psllw.java
@@ -77,7 +77,7 @@ public class Psllw extends CalvisAnimation {
 //        BigInteger biResult = biDes;
 
         String carryFlagValue = "";
-        for (int x = 1; x <= counter; x++) {
+        for (int x = 1; x <= counter * 4; x++) {
             biW4 = biW4.shiftLeft(1);
             biW4 = biW4.clearBit(0);
 
@@ -90,49 +90,46 @@ public class Psllw extends CalvisAnimation {
                 rotateModels4.add(new RotateModel(c.binaryZeroExtend(biW4.toString(2), operandSize), carryFlagValue));
         }
 
-        for (int x = 1; x <= counter; x++) {
-            biW3 = biW3.shiftLeft(1);
-            biW3 = biW3.clearBit(0);
-
-            if (operandSize < c.binaryZeroExtend(biW3.toString(2), operandSize).length()) {
-                int cut = c.binaryZeroExtend(biW3.toString(2), operandSize).length() - operandSize;
-                String t = c.binaryZeroExtend(biW3.toString(2), operandSize).substring(cut);
-                rotateModels3.add(new RotateModel(t, carryFlagValue));
-            }
-            else
-                rotateModels3.add(new RotateModel(c.binaryZeroExtend(biW3.toString(2), operandSize), carryFlagValue));
-        }
-
-        for (int x = 1; x <= counter; x++) {
-            biW2 = biW2.shiftLeft(1);
-            biW2 = biW2.clearBit(0);
-
-            if (operandSize < c.binaryZeroExtend(biW2.toString(2), operandSize).length()) {
-                int cut = c.binaryZeroExtend(biW2.toString(2), operandSize).length() - operandSize;
-                String t = c.binaryZeroExtend(biW2.toString(2), operandSize).substring(cut);
-                rotateModels2.add(new RotateModel(t, carryFlagValue));
-            }
-            else
-                rotateModels2.add(new RotateModel(c.binaryZeroExtend(biW2.toString(2), operandSize), carryFlagValue));
-        }
-
-        for (int x = 1; x <= counter; x++) {
-            biW1 = biW1.shiftLeft(1);
-            biW1 = biW1.clearBit(0);
-
-            if (operandSize < c.binaryZeroExtend(biW1.toString(2), operandSize).length()) {
-                int cut = c.binaryZeroExtend(biW1.toString(2), operandSize).length() - operandSize;
-                String t = c.binaryZeroExtend(biW1.toString(2), operandSize).substring(cut);
-                rotateModels1.add(new RotateModel(t, carryFlagValue));
-            }
-            else
-                rotateModels1.add(new RotateModel(c.binaryZeroExtend(biW1.toString(2), operandSize), carryFlagValue));
-        }
+//        for (int x = 1; x <= counter; x++) {
+//            biW3 = biW3.shiftLeft(1);
+//            biW3 = biW3.clearBit(0);
+//
+//            if (operandSize < c.binaryZeroExtend(biW3.toString(2), operandSize).length()) {
+//                int cut = c.binaryZeroExtend(biW3.toString(2), operandSize).length() - operandSize;
+//                String t = c.binaryZeroExtend(biW3.toString(2), operandSize).substring(cut);
+//                rotateModels3.add(new RotateModel(t, carryFlagValue));
+//            }
+//            else
+//                rotateModels3.add(new RotateModel(c.binaryZeroExtend(biW3.toString(2), operandSize), carryFlagValue));
+//        }
+//
+//        for (int x = 1; x <= counter; x++) {
+//            biW2 = biW2.shiftLeft(1);
+//            biW2 = biW2.clearBit(0);
+//
+//            if (operandSize < c.binaryZeroExtend(biW2.toString(2), operandSize).length()) {
+//                int cut = c.binaryZeroExtend(biW2.toString(2), operandSize).length() - operandSize;
+//                String t = c.binaryZeroExtend(biW2.toString(2), operandSize).substring(cut);
+//                rotateModels2.add(new RotateModel(t, carryFlagValue));
+//            }
+//            else
+//                rotateModels2.add(new RotateModel(c.binaryZeroExtend(biW2.toString(2), operandSize), carryFlagValue));
+//        }
+//
+//        for (int x = 1; x <= counter; x++) {
+//            biW1 = biW1.shiftLeft(1);
+//            biW1 = biW1.clearBit(0);
+//
+//            if (operandSize < c.binaryZeroExtend(biW1.toString(2), operandSize).length()) {
+//                int cut = c.binaryZeroExtend(biW1.toString(2), operandSize).length() - operandSize;
+//                String t = c.binaryZeroExtend(biW1.toString(2), operandSize).substring(cut);
+//                rotateModels1.add(new RotateModel(t, carryFlagValue));
+//            }
+//            else
+//                rotateModels1.add(new RotateModel(c.binaryZeroExtend(biW1.toString(2), operandSize), carryFlagValue));
+//        }
 
         ArrayList<Text> rotateResults = new ArrayList<>();
-//        ArrayList<Text> rotateResults3 = new ArrayList<>();
-//        ArrayList<Text> rotateResults2 = new ArrayList<>();
-//        ArrayList<Text> rotateResults1 = new ArrayList<>();
         ArrayList<Text> zeroFlags = new ArrayList<>();
         ArrayList<Text> resultLabel = new ArrayList<>();
         ArrayList<Text> cfLabel = new ArrayList<>();
@@ -140,51 +137,22 @@ public class Psllw extends CalvisAnimation {
         Text resultText = timeFunc.generateText(new Text("Final Result"), 13, "#3d2b1f", FontWeight.NORMAL, "Elephant");
         Rectangle fake = timeFunc.createRectangle(0, 0, Color.WHITE);
         parent.add(fake);
-
         for(int x = 0; x < counter; x++){
             rotateResults.add(timeFunc.generateText(new Text(rotateModels4.get(x).getResult()), 13, "#5d8aa8", FontWeight.NORMAL, "Elephant"));
             numberLabel.add(timeFunc.generateText(new Text(Integer.toString(x + 1)), 13, "#a4c639", FontWeight.NORMAL, "Elephant"));
             zeroFlags.add(timeFunc.generateText(new Text(rotateModels4.get(x).getFlag()), 13, "#5d8aa8", FontWeight.NORMAL, "Elephant"));
-            resultLabel.add(timeFunc.generateText(new Text(tokens[0].getValue() ), 13, "#3d2b1f", FontWeight.NORMAL, "Elephant"));
-            cfLabel.add(timeFunc.generateText(new Text("word 4"), 13, "#3d2b1f", FontWeight.NORMAL, "Elephant"));
+            resultLabel.add(timeFunc.generateText(new Text(tokens[0].getValue()), 13, "#3d2b1f", FontWeight.NORMAL, "Elephant"));
+            cfLabel.add(timeFunc.generateText(new Text(""), 13, "#3d2b1f", FontWeight.NORMAL, "Elephant"));
             parent.addAll(rotateResults.get(x), zeroFlags.get(x), cfLabel.get(x), resultLabel.get(x), numberLabel.get(x));
         }
 
         for(int x = 0; x < counter; x++){
-            rotateResults.add(timeFunc.generateText(new Text(rotateModels3.get(x).getResult()), 13, "#5d8aa8", FontWeight.NORMAL, "Elephant"));
-            numberLabel.add(timeFunc.generateText(new Text(Integer.toString(x + 1)), 13, "#a4c639", FontWeight.NORMAL, "Elephant"));
-            zeroFlags.add(timeFunc.generateText(new Text(rotateModels3.get(x).getFlag()), 13, "#5d8aa8", FontWeight.NORMAL, "Elephant"));
-            resultLabel.add(timeFunc.generateText(new Text(tokens[0].getValue() + "(word 3)"), 13, "#3d2b1f", FontWeight.NORMAL, "Elephant"));
-            cfLabel.add(timeFunc.generateText(new Text("word 3"), 13, "#3d2b1f", FontWeight.NORMAL, "Elephant"));
-            parent.addAll(rotateResults.get(x), zeroFlags.get(x), cfLabel.get(x), resultLabel.get(x), numberLabel.get(x));
-        }
-
-        for(int x = 0; x < counter; x++){
-            rotateResults.add(timeFunc.generateText(new Text(rotateModels2.get(x).getResult()), 13, "#5d8aa8", FontWeight.NORMAL, "Elephant"));
-            numberLabel.add(timeFunc.generateText(new Text(Integer.toString(x + 1)), 13, "#a4c639", FontWeight.NORMAL, "Elephant"));
-            zeroFlags.add(timeFunc.generateText(new Text(rotateModels2.get(x).getFlag()), 13, "#5d8aa8", FontWeight.NORMAL, "Elephant"));
-            resultLabel.add(timeFunc.generateText(new Text(tokens[0].getValue() + "(word 2)"), 13, "#3d2b1f", FontWeight.NORMAL, "Elephant"));
-            cfLabel.add(timeFunc.generateText(new Text("word 2"), 13, "#3d2b1f", FontWeight.NORMAL, "Elephant"));
-            parent.addAll(rotateResults.get(x), zeroFlags.get(x), cfLabel.get(x), resultLabel.get(x), numberLabel.get(x));
-        }
-
-        for(int x = 0; x < counter; x++){
-            rotateResults.add(timeFunc.generateText(new Text(rotateModels2.get(x).getResult()), 13, "#5d8aa8", FontWeight.NORMAL, "Elephant"));
-            numberLabel.add(timeFunc.generateText(new Text(Integer.toString(x + 1)), 13, "#a4c639", FontWeight.NORMAL, "Elephant"));
-            zeroFlags.add(timeFunc.generateText(new Text(rotateModels2.get(x).getFlag()), 13, "#5d8aa8", FontWeight.NORMAL, "Elephant"));
-            resultLabel.add(timeFunc.generateText(new Text(tokens[0].getValue() + "(word 1)"), 13, "#3d2b1f", FontWeight.NORMAL, "Elephant"));
-            cfLabel.add(timeFunc.generateText(new Text("word 1"), 13, "#3d2b1f", FontWeight.NORMAL, "Elephant"));
-            parent.addAll(rotateResults.get(x), zeroFlags.get(x), cfLabel.get(x), resultLabel.get(x), numberLabel.get(x));
-        }
-
-        for(int x = 0; x < counter * 4; x++){
             timeFunc.setTimelinePosition(60 + rotateResults.get(x).getLayoutBounds().getWidth(), 20 + 50 * x, zeroFlags.get(x));
             timeFunc.setTimelinePosition(20 + 30, 20 + 50 * x, rotateResults.get(x));
             timeFunc.setTimelinePosition(60 + rotateResults.get(x).getLayoutBounds().getWidth() , 40 + 50 * x, cfLabel.get(x));
             timeFunc.setTimelinePosition(20 + 30 , 40 + 50 * x, resultLabel.get(x));
             timeFunc.setTimelinePosition(10 , 20 + 50 * x, numberLabel.get(x));
         }
-
         parent.add(resultText);
         timeFunc.setTimelinePosition(20 + 60 + resultLabel.get(0).getLayoutBounds().getWidth(), 40 + 50 * (counter  - 1), resultText);
         timeline.play();
