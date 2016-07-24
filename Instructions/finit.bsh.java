96da80ba4a20be6f5de87f7db27667b6e0c513f5
GoodwinChua <chua.goodwin@gmail.com>
2016-07-24 20:47:27 +0800

    FPU conversion fixed fcom, fucom

--- Instructions/finit.bsh.java
+++ Instructions/finit.bsh.java
@@ -1,5 +1,13 @@
 execute(registers, memory) {
-    registers.x87().status.initializeValue();
-    registers.x87().control.initializeValue();
-    registers.x87().tag.initializeValue();
+  int top = registers.x87().status().getTop();
+
+  while ( top < 8 ) {
+    top++;
+    registers.x87().rotateBarrel(1); // rotate right to simulate pop
+  }
+
+  registers.x87().status().initializeValue();
+  registers.x87().control().initializeValue();
+  registers.x87().tag().initializeValue();
+  registers.x87().refreshST();
 }
