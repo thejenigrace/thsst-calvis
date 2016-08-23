65ffd127f0598c379cd5add14421661b57be253b
Jennica Alcalde <jennica.alcalde@gmail.com>
2016-08-23 19:15:02 +0800

    Ivan fix

--- Instructions/modpush.bsh.java
+++ Instructions/modpush.bsh.java
@@ -1,4 +1,5 @@
-execute(src, registers, memory) throws Exception {
+execute(des, src, registers, memory) throws Exception {
+	String memAddress = des.getValue();
 	int size = Memory.MAX_ADDRESS_SIZE; // 32
 	String stackPointer = registers.getStackPointer();
 	String val = "";
@@ -6,18 +7,21 @@ execute(src, registers, memory) throws Exception {
 	if ( src.isRegister() ) {
 		size = registers.getBitSize(src);
 		val = registers.get(src);
-
+		val = new BigInteger(val, 16).toString(10);
+		System.out.println("val " + val);
 		char[] chars = val.toCharArray();
 		for (int i = 0; i < chars.length; i++)
 		{
 			sbHex.append(Integer.toHexString((int) chars[i]));
 		}
-		System.out.println(sbHex.toString() + " result");
-		System.out.println(val + " val result");
+//		System.out.println(sbHex.toString() + " result");
 	}
 
+	memory.write(des, sbHex.toString(), size * 2);
+	System.out.println(memory.read(des, size * 2));
+
 	BigInteger stackAddress = new BigInteger(stackPointer, 16);
-	BigInteger offset = new BigInteger(size * 2 + "");
+	BigInteger offset = new BigInteger((size / 2) + "");
 	offset = offset.divide(new BigInteger("8"));
 	stackAddress = stackAddress.subtract(offset);
 
@@ -26,6 +30,6 @@ execute(src, registers, memory) throws Exception {
 	} else {
 		registers.setStackPointer(stackAddress.toString(16));
 		String stackEntry = registers.getStackPointer();
-		memory.writeToStack(stackEntry, sbHex.toString(), size * 2);
+		memory.writeToStack(stackEntry, memAddress, size);
 	}
 }
